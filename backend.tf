d9aadc581f9ebcba4ba86e279b47e97aa420926f
cloudcontainer-githu <githubabc800@gmail.com>
2025-07-18 15:38:47 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket        = "cloudcontainer-terraform-state-1a"
-    key           = "terraform.tfstate"
-    region        = "ap-south-1"
-    encrypt       = true
-   dynamodb_table = "cloudcontainer-tf-table"
+    bucket         = "cloudcontainer-terraform-state-1a"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"
+    profile        = "my-aws-profile"
   }
 }
