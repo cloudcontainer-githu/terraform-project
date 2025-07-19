5f0ee75de10d1c7de164c4f4b4a4d7285c12b87d
cloudcontainer-githu <githubabc800@gmail.com>
2025-07-20 00:34:57 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -37,6 +37,13 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
+# ✅ Added: Associate public subnets with the public route table
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = length(aws_subnet.public_subnet)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
 resource "aws_subnet" "private_subnet" {
   count             = length(var.private_subnets_cidr)
   vpc_id            = aws_vpc.main.id
@@ -79,4 +86,3 @@ resource "aws_route_table_association" "private_subnet_association" {
   subnet_id      = aws_subnet.private_subnet[count.index].id
   route_table_id = aws_route_table.private_route_table.id
 }
-
