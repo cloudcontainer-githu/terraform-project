fd83eb41169fa08dc215595d1e769029ccc3d0b3
cloudcontainer-githu <githubabc800@gmail.com>
2025-07-19 18:14:45 +0530

    Update main.tf

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -57,6 +57,12 @@ resource "aws_security_group" "frontend_sg" {
     security_groups = [aws_security_group.bastion_sg.id]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.anywhere
+  }
 }
 
 resource "aws_security_group" "backend_sg" {
@@ -97,5 +103,11 @@ resource "aws_security_group" "rds_sg" {
     protocol        = "tcp"
     security_groups = [aws_security_group.backend_sg.id]
   }
-}
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.anywhere
+  }
+}
